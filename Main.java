c3e946111c19b59d15da2dc7f737ba15c4f6062f
Michael <michaelkivo977@gmail.com>
2024-11-16 15:40:07 +0300

    Modify Main.java: add 2 print

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("Добавил 1");
+        System.out.println("Добавил 2");
     }
 }
